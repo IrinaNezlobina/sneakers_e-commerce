13c7b6757171d63abfbd125099092207ab14f2bf
Irina <nezlobina_698@mail.ru>
2024-02-09 18:31:12 +0300

    drawer

---
+++ src/components/CartItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="">
+    <div class="flex flex-col w-full">
+        <p>sneakers</p>
+    <div class="flex  justify-between mt-2">
+        <b>12990 </b>
+        <img class="opacity-40 hover:opacity-100 cursor-pointer transition" src="/close.svg" alt="">
+    </div>
+    </div>
+  </div>
+  </template>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,11 +2,34 @@
   <div class="fixed top-0 left-0 w-full h-full bg-black opacity-50"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
  <DrawerHead/>
+ <CartItemList/>
+
+ <div class="flex gap-4 mb-6 mt-7-7 flex-col">
+  <div class="flex gap-2">
+    <span>
+      Итого
+      <div class="flex-1 border-b border-dashed"></div>
+    </span>
+    <b>1290</b>
+  </div>
+  <div class="flex gap-2">
+    <span>
+      Налог 5%
+      <div class="flex-1 border-b border-dashed"></div>
+    </span>
+    <b>990</b>
+  </div>
+   <button disabled class="mt-4 cursor-pointer bg-lime-500 w-full text-white hover:bg-lime-600 transition active:bg-lime-700 disabled:bg-slate-300  rounded-xl py-3">
+  Оформить
+ </button>
+ </div>
+
   </div>
 </template>
 
 <script setup>
 import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue';
 </script>
 
 <style scoped></style>
